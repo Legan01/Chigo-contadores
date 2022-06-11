1ec5b16ad6ade5c486988c9a6550d51e708046d3
Legan01 <LuisApolinar@outlook.com>
2022-06-11 16:31:56 -0700

    nueva-actualizacionv2

--- app/app.js
+++ app/app.js
@@ -38,6 +38,7 @@
 */
 /*romper el preloader */
 
+
 const list = document.querySelectorAll(".list");
 
 function activeLink() {
@@ -74,8 +75,7 @@ toggle.addEventListener('click', cambiarTheme);
 
 
 
-
-
+/*
 
 
 document.getElementById("boton").addEventListener("click",e=>{
@@ -195,13 +195,165 @@ document.getElementById("administracion").addEventListener("click",e=>{
   });
 });
 
+*/
+
+/*pruebas */
 
 
 
+window.onload = function() {
+  let pricing = document.getElementById("pricing");
+  document.getElementById("asesoria").onclick = function() {
+    Swal.fire({
 
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Asesoria fiscal",
+      html:
+        "asesoramiento para ayudarle en las decisiones que debas tomar en materia de Finanzas, con el objetivo de que puedas rentabilizar cada vez mas tus recursos financieros.",
+      showCloseButton: true,
 
+    });
+  }
+  document.getElementById("contabilidad").onclick = function() {
+    Swal.fire({
 
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Contabilidad General",
+      html:
+        "Asesoramiento en las operaciones diarias para la toma de decisiones a través de Estados Financieros como Persona Física o Moral.",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+  document.getElementById('administracion').onclick = function(){
+    Swal.fire({
 
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Administracion de Nominas",
+      html:
+        "asesoramiento y elaboración puntual de los comprobantes de nómina, elaboración de finiquitos, calculo de vacaciones, primas vacacionales, aguinaldo, tiempo extra, etc.",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+  document.getElementById('Planificación').onclick = function(){
+    Swal.fire({
 
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Planificación fiscal y financiera",
+      html:
+        "Combinar el know-how con la planificación estratégica es una solución eficaz para ayudar a los clientes a alcanzar sus objetivos financieros. Este proceso implica algunas tareas simples, como: <br> Organizar sesiones de planificación; <br>Ayudar a diseñar el plan financiero del cliente para el próximo año.        ",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+  document.getElementById('control').onclick = function(){
+    Swal.fire({
 
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Control Interno",
+      html:
+        "Revisiónes de procesos de control.    ",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
 
+  document.getElementById('impuestos').onclick = function(){
+    Swal.fire({
+
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Control Interno",
+      html:
+        "Impuesto al Valor Agregado, Impuesto Sobre la Renta, Impuesto Sobre Nómina, etc.  ",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+  document.getElementById('impuestos').onclick = function(){
+    Swal.fire({
+
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Control Interno",
+      html:
+        "Impuesto al Valor Agregado, Impuesto Sobre la Renta, Impuesto Sobre Nómina, etc.  ",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+  document.getElementById('impuestos').onclick = function(){
+    Swal.fire({
+
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Control Interno",
+      html:
+        "Impuesto al Valor Agregado, Impuesto Sobre la Renta, Impuesto Sobre Nómina, etc.  ",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+  document.getElementById('consultaria_em').onclick = function(){
+    Swal.fire({
+
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: "Consultoría empresarial",
+      html:
+        "Las empresas buscan asesoramiento empresarial para aumentar su productividad y eficacia, pero la mayoría de los servicios de contabilidad se enfocan en tareas más básicas, cuya demanda es cada vez menor. Además, gracias al avance de la tecnología, estos servicios básicos se pueden llevar a cabo de manera automática. ",
+      showCloseButton: true,
+      confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ok!',
+      confirmButtonAriaLabel: "Thumbs up, great!",
+    });
+  }
+}
